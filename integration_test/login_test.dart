9892cebc846e78ba89d024ece72bef95fae4f3c7
Misheel31 <misheelrai7@gmail.com>
2025-02-22 21:53:06 +0545

    Login integration test

---
+++ integration_test/login_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:home_rental/features/home/presentation/widget/location_search.dart';
+import 'package:home_rental/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('LoginView Test', (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    expect(find.text('Welcome Back!'), findsOneWidget);
+
+    final emailField = find.byType(TextFormField).at(0);
+    final passwordField = find.byType(TextFormField).at(1);
+    final loginButton = find.byType(ElevatedButton);
+
+    expect(emailField, findsOneWidget);
+    expect(passwordField, findsOneWidget);
+    expect(loginButton, findsOneWidget);
+
+    await tester.enterText(emailField, 'abc@gmail.com');
+    await tester.enterText(passwordField, '123456789');
+
+    await tester.tap(loginButton);
+    await tester.pump();
+    await tester.pumpAndSettle();
+
+    expect(find.byType(Scaffold), findsOneWidget);
+
+    expect(
+        find.byWidgetPredicate(
+          (widget) =>
+              widget is AppBar &&
+              widget.title is Text &&
+              (widget.title as Text).data == 'Rentify',
+        ),
+        findsOneWidget);
+
+    expect(find.byType(LocationSearch), findsOneWidget);
+  });
+}
